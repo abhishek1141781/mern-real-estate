052f9bb461020b5e1026a88ccfe05c4e82a5c9a3
Abhishek Gupta <abhishek1141781@gmail.com>
2024-02-07 01:42:43 +0530

    added vercel setup

--- api/index.js
+++ api/index.js
@@ -5,23 +5,27 @@ import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import path form 'path';
 
 dotenv.config();
+
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("connected to mongoose");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+.connect(process.env.MONGODB_URI)
+.then(() => {
+  console.log("connected to mongoose");
+})
+.catch((err) => {
+  console.log(err);
+});
+
+const __dirname = path.resolve();
 
 app.listen(3000, () => {
   console.log("server runing on 3000");
@@ -31,6 +35,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res) => {
+  res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'))
+})
+
 // error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -38,6 +48,6 @@ app.use((err, req, res, next) => {
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message
+    message,
   });
 });
